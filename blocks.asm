c90ef1868ff74ba7c9c584258c425e2da37a5b09
Samuel Pagenkopf <ssaammp@gmail.com>
2018-04-25 13:41:55 -0500

    fill middle of board with black

--- blocks.asm
+++ blocks.asm
@@ -98,13 +98,17 @@ draw_top:
 @pos = board_pos - $20
         lda #>@pos
         ldx #<@pos
-        jsr draw_border_row
-        rts
+        bne draw_border_row     ; hi pos always nonzero
 draw_bottom:
 @pos = board_pos + $20*20
         lda #>@pos
         ldx #<@pos
-        jsr draw_border_row
+
+draw_border_row:
+        ldy #2          ; border tile
+        sty 0
+        ldy #12         ; width
+        jsr fill_row
 go_back:
         rts
 
@@ -118,12 +122,12 @@ update_tiles:
         beq draw_top
         
 draw_border:
-;; a is the timer, set to the y position of the board
-;; determine high address byte
+;; a is the timer, range 21..2
         clc
-        adc #board_y-2
+        adc #board_y-2  ; move timer into y position
         ldx #$1f        ; base address
         tay             ; save a
+;; determine high address byte
         sec
 @loop:  inx
         sbc #$8
@@ -138,24 +142,34 @@ draw_border:
 ;; draw tiles
         clc
         adc #board_x
+        sta 0           ; swap bytes
+        txa
+        ldx 0
+;; left edge
         ldy #$2         ; border tile
-        stx $2006       ; left edge
         sta $2006
-        sty $2007
-
-        adc #11         ; right edge 
         stx $2006
+        sty $2007
+;; middle (call fill_row)
+        inx
+        ldy #0          ; blank tile
+        sty 0
+        ldy #10         ; fill width
+        jsr fill_row
+;; right edge
+        ldy #$2         ; border tile
         sta $2006
+        stx $2006
         sty $2007
         rts
 
-draw_border_row:
-        ldy #12
+;; input: a - addr hi, x - addr lo, y - width, 0 - fill with
+fill_row:
 @loop:
         sta $2006
         stx $2006
         pha
-        lda #$2         ; border tile
+        lda 0          ; border tile
         sta $2007
         pla
         inx
