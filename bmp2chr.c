790046ab69245606c02cae721794409aa2b7e6ff
Samuel Pagenkopf <ssaammp@gmail.com>
2018-04-28 10:43:00 -0500

    more flexible bmp2chr

--- bmp2chr.c
+++ bmp2chr.c
@@ -1,11 +1,6 @@
 #include <SDL2/SDL.h>
 #include <assert.h>
 
-enum {
-        CHR_WIDTH = 128,
-        CHR_SIZE = 128 * 128 * 2,
-};
-
 int main(int argc, char **argv)
 {
         SDL_Init(SDL_INIT_VIDEO);
@@ -15,18 +10,18 @@ int main(int argc, char **argv)
         FILE *out = fopen(outpath, "w");
 
         char *bmp = s->pixels;
-        Uint64 *buf = calloc(1, CHR_SIZE / 8);
-        assert(s->w == CHR_WIDTH && s->h == CHR_WIDTH);
-        for (int t=0; t<256; ++t) {
-                char *tile = &bmp[(t%16)*8 + (t/16)*CHR_WIDTH*8];
-                for (int p=0; p<64; ++p) {
-                        long b = tile[(7-p%8) + (p/8)*CHR_WIDTH];
+        const size_t bufsize = s->w * s->h * 2 / 8;
+        Uint64 *buf = calloc(1, bufsize);
+        unsigned tiles_x = s->w / 8;
+        for (unsigned t=0; t<bufsize/64; ++t) {
+                char *tile = &bmp[(t%tiles_x)*8 + (t/tiles_x)*s->w*8];
+                for (unsigned p=0; p<64; ++p) {
+                        long b = tile[(7-p%8) + (p/8)*s->w];
                         buf[t*2+0] |= (b >> 0 & 1) << p;
                         buf[t*2+1] |= (b >> 1 & 1) << p;
                 }
         }
-
-        fwrite(buf, 1, CHR_SIZE / 8, out);
+        fwrite(buf, 1, bufsize, out);
 
         fclose(out);
         SDL_Quit();
