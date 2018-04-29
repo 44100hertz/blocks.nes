1735a4b6f9c1ecaec0e738265b8094110d1f2cd8
Samuel Pagenkopf <ssaammp@gmail.com>
2018-04-29 01:12:36 -0500

    display timer

--- blocks.asm
+++ blocks.asm
@@ -8,21 +8,19 @@
 .word vblank, reset, 0
 
 .segment "ZEROPAGE"
+                        .res 8  ;used for function calls
 update_done:            .res 1
 scroll:                 .res 1
 init_board_timer:       .res 1
 clear_screen_timer:     .res 1
-timer:
-time_hours:             .res 1
-time_mins:              .res 1
-time_secs:              .res 1
-time_frames:            .res 1
 
 t_blank  = 0
 t_block  = 1
-t_border = 2
+t_border = 4
 
 .segment "CODE"
+
+timer = $200
 
 ;; uses: x
 set_scroll_and_flags:
@@ -44,7 +42,7 @@ set_ppu_flags:
 update_colors:
         sta 0
 @loop:
-        lda #$3f        ; colors are on 3fxx
+        lda #$3f
         sta $2006
         stx $2006
         lda palettes,y
@@ -57,6 +55,9 @@ update_colors:
 
 palettes:
 .byte $01, $14, $24, $34
+.byte $11, $2c, $3c
+.byte $16, $27, $38
+.byte $1d, $2d, $3d
 
 reset:
         sei
@@ -83,25 +84,28 @@ reset:
 main:
         ldx #0          ; "I'm not done yet"
         stx update_done
-
-update_timer:
-        lda #0
-        ldx #4
-@loop:
-        inc timer,x
-        ldy timer,x
-        cpy #60
-        bne @done
-        sta timer,x
-        dex
-        bne @loop
-@done:
+        jsr update_timer
 
         ldx #1          ; "Ok we're done now"
         stx update_done
 :       jmp :-          ; spin until vblank
 
-
+timer_limits:
+;;    hours       minutes     seconds     frames (change to '5' for pal)
+.byte $ff, '9'+1, '6', '9'+1, '6', '9'+1, '6', '9'+1 
+update_timer:
+        ldx #8
+@loop:
+        inc timer-1,x
+        lda timer-1,x
+        cmp timer_limits-1,x
+        bne @done
+        lda #'0'
+        sta timer-1,x
+        dex
+        bne @loop
+@done:
+        rts
 
 vblank:
         pha             ; save a
@@ -116,20 +120,38 @@ no_return:              ; done with update
         pla
         pla
 
+        jsr draw_timer
         jsr update_tiles
         jsr set_scroll_and_flags
         jmp main
+
+draw_timer:
+timer_x = board_x
+timer_y = board_y+21
+timer_pos = $2000 + timer_y*$20 + timer_x
+        ldy #>timer_pos
+        ldx #<timer_pos
+@loop:
+        sty $2006
+        stx $2006
+        lda timer - <timer_pos,x
+        sta $2007
+        inx
+        cpx #<timer_pos+8
+        bne @loop
+        rts
 
 board_y = 5
 board_x = 5
 board_pos = $2000 + board_y*$20 + board_x
 
 init_board:
-        ldx #0
-        stx time_frames
-        stx time_secs
-        stx time_mins
-        stx time_hours
+        ldy #8
+        lda #'0'
+@loop:
+        sta timer-1,y
+        dey
+        bne @loop
         ldx #$1e
         stx clear_screen_timer
         ldx #22
